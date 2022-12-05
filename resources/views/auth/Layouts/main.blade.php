821ca611ff890397018a27aa5a58cad746972238
SamuelNusanetID <samuel@nusa.net.id>
2022-12-05 20:27:36 +0700

    Fix: Perbaikan UI Tampilan Login dan Register

--- resources/views/auth/Layouts/main.blade.php
+++ resources/views/auth/Layouts/main.blade.php
@@ -1,43 +1,70 @@
-<!DOCTYPE html>
-<html lang="en">
+<!doctype html>
+<html lang="en" dir="ltr">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
-        integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="{{ URL::to('plugin/bs522/css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ URL::to('bin/css/style.css') }}">
-
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Admin Form | {{ $titlePage }}</title>
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="{{ URL::to('plugin/hopeui/assets/images/favicon.ico') }}" />
+    <!-- Library / Plugin Css Build -->
+    <link rel="stylesheet" href="{{ URL::to('plugin/hopeui/assets/css/core/libs.min.css') }}" />
+    <!-- Hope Ui Design System Css -->
+    <link rel="stylesheet" href="{{ URL::to('plugin/hopeui/assets/css/hope-ui.min.css?v=1.2.0') }}" />
+    <!-- Custom Css -->
+    <link rel="stylesheet" href="{{ URL::to('plugin/hopeui/assets/css/custom.min.css?v=1.2.0') }}" />
+    <!-- Dark Css -->
+    <link rel="stylesheet" href="{{ URL::to('plugin/hopeui/assets/css/dark.min.css') }}" />
+    <!-- Customizer Css -->
+    <link rel="stylesheet" href="{{ URL::to('plugin/hopeui/assets/css/customizer.min.css') }}" />
+    <!-- RTL Css -->
+    <link rel="stylesheet" href="{{ URL::to('plugin/hopeui/assets/css/rtl.min.css') }}" />
 </head>
 
-<body>
-    <div class="container-fluid d-flex align-items-center justify-content-center vh-100">
-        <div class="container-fluid">
-            <div class="text-center">
-                <div class="d-flex align-items-center justify-content-center mb-5">
-                    <div class="wrapper-image" style="box-shadow: 5px 6px rgba(0,0,0,.2);">
-                        <img class="img-fluid" src="{{ URL::to('bin/img/0DaWevkh_400x400-removebg-preview.png') }}"
-                            alt="" width="80" height="80">
-                    </div>
-                </div>
+<body class=" " data-bs-spy="scroll" data-bs-target="#elements-section" data-bs-offset="0" tabindex="0">
+    <!-- loader Start -->
+    <div id="loading">
+        <div class="loader simple-loader">
+            <div class="loader-body"></div>
+        </div>
+    </div>
+    <!-- loader END -->
 
-                <div class="d-flex align-items-center justify-content-center">
-                    <div class="card bg-white" style="width: 25em;">
-                        <div class="card-body text-start">
-                            <h3 class="fw-bold text-center text-success">{{ $headerCard }}</h3>
+    <div class="wrapper">
+        <section class="login-content">
+            <div class="row m-0 align-items-center bg-light vh-100">
+                <div class="col-12">
+                    <div class="row justify-content-center">
+                        <div class="col-md-6">
                             @yield('auth-wrapper')
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </section>
     </div>
 
-    <script src="{{ URL::to('plugin/bs522/js/bootstrap.bundle.min.js') }}"></script>
+    <!-- Library Bundle Script -->
+    <script src="{{ URL::to('plugin/hopeui/assets/js/core/libs.min.js') }}"></script>
+    <!-- External Library Bundle Script -->
+    <script src="{{ URL::to('plugin/hopeui/assets/js/core/external.min.js') }}"></script>
+    <!-- Widgetchart Script -->
+    <script src="{{ URL::to('plugin/hopeui/assets/js/charts/widgetcharts.js') }}"></script>
+    <!-- mapchart Script -->
+    <script src="{{ URL::to('plugin/hopeui/assets/js/charts/vectore-chart.js') }}"></script>
+    <script src="{{ URL::to('plugin/hopeui/assets/js/charts/dashboard.js') }}"></script>
+    <!-- fslightbox Script -->
+    <script src="{{ URL::to('plugin/hopeui/assets/js/plugins/fslightbox.js') }}"></script>
+    <!-- Settings Script -->
+    <script src="{{ URL::to('plugin/hopeui/assets/js/plugins/setting.js') }}"></script>
+    <!-- Slider-tab Script -->
+    <script src="{{ URL::to('plugin/hopeui/assets/js/plugins/slider-tabs.js') }}"></script>
+    <!-- Form Wizard Script -->
+    <script src="{{ URL::to('plugin/hopeui/assets/js/plugins/form-wizard.js') }}"></script>
+    <!-- AOS Animation Plugin-->
+    <!-- App Script -->
+    <script src="{{ URL::to('plugin/hopeui/assets/js/hope-ui.js') }}" defer></script>
 </body>
 
 </html>
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,121 +1,136 @@
 @extends('auth.Layouts.main')
 
 @section('auth-wrapper')
-    <p class="text-center text-success">Silahkan masuk untuk memulai</p>
-    <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
-        <symbol id="check-circle-fill" viewBox="0 0 16 16">
-            <path
-                d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
-        </symbol>
-        <symbol id="info-fill" viewBox="0 0 16 16">
-            <path
-                d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z" />
-        </symbol>
-        <symbol id="exclamation-triangle-fill" viewBox="0 0 16 16">
-            <path
-                d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
-        </symbol>
-    </svg>
-    @if (session()->has('successMessage'))
-        <div class="alert alert-success alert-dismissible d-flex align-items-center fade show" role="alert">
-            <svg width="24" height="24" fill="currentColor" class="bi flex-shrink-0 me-2" role="img"
-                viewBox="0 0 16 16" aria-label="Success:">
-                <use xlink:href="#check-circle-fill" />
-            </svg>
-            <div style="text-align: justify;">
-                {{ session('successMessage') }}
-            </div>
-            <div>
-                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-            </div>
-        </div>
-    @endif
-    @error('successMessage')
-        <div class="alert alert-success alert-dismissible d-flex align-items-center fade show" role="alert">
-            <svg width="24" height="24" fill="currentColor" class="bi flex-shrink-0 me-2" role="img"
-                viewBox="0 0 16 16" aria-label="Success:">
-                <use xlink:href="#check-circle-fill" />
-            </svg>
-            <div style="text-align: justify;">
-                {{ $message }}
-            </div>
-            <div>
-                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-            </div>
-        </div>
-    @enderror
+    <div class="card card-transparent shadow d-flex justify-content-center mb-0 auth-card">
+        <div class="card-body">
+            <a href="{{ URL::to('login') }}" class="navbar-brand d-flex align-items-center mb-3">
+                <!--Logo start-->
+                <img src="{{ URL::to('bin/img/0DaWevkh_400x400-removebg-preview.png') }}" alt="" width="30">
+                <!--logo End-->
+                <h4 class="logo-title ms-3 fw-bold">N U S A N E T</h4>
+            </a>
+            <h2 class="mb-2 text-center">Dashboard Admin</h2>
+            <p class="text-center mb-2">M A S U K</p>
 
-    @if (session()->has('errorMessage'))
-        <div class="alert alert-danger alert-dismissible d-flex align-items-center fade show" role="alert">
-            <svg width="24" height="24" fill="currentColor" class="bi flex-shrink-0 me-2" role="img"
-                viewBox="0 0 16 16" aria-label="Success:">
-                <use xlink:href="#exclamation-triangle-fill" />
+            <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+                <symbol id="check-circle-fill" viewBox="0 0 16 16">
+                    <path
+                        d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
+                </symbol>
+                <symbol id="info-fill" viewBox="0 0 16 16">
+                    <path
+                        d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z" />
+                </symbol>
+                <symbol id="exclamation-triangle-fill" viewBox="0 0 16 16">
+                    <path
+                        d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+                </symbol>
             </svg>
-            <div style="text-align: justify;">
-                {{ session('errorMessage') }}
-            </div>
-            <div>
-                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-            </div>
-        </div>
-    @endif
-    @error('errorMessage')
-        <div class="alert alert-danger alert-dismissible d-flex align-items-center fade show" role="alert">
-            <svg width="24" height="24" fill="currentColor" class="bi flex-shrink-0 me-2" role="img"
-                viewBox="0 0 16 16" aria-label="Success:">
-                <use xlink:href="#exclamation-triangle-fill" />
-            </svg>
-            <div style="text-align: justify;">
-                {{ $message }}
-            </div>
-            <div>
-                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-            </div>
-        </div>
-    @enderror
-    <form action="{{ URL::to('login') }}" method="POST">
-        @csrf
-        <div class="mb-3">
-            <label for="email" class="form-label text-success">Email</label>
-            <input type="email" class="form-control @error('email') is-invalid @enderror" id="email" name="email"
-                placeholder="Masukkan email anda...">
-            @error('email')
-                <div class="invalid-feedback">
-                    {{ $message }}
+            @if (session()->has('successMessage'))
+                <div class="alert alert-success alert-dismissible d-flex align-items-center fade show" role="alert">
+                    <svg width="24" height="24" fill="currentColor" class="bi flex-shrink-0 me-2" role="img"
+                        viewBox="0 0 16 16" aria-label="Success:">
+                        <use xlink:href="#check-circle-fill" />
+                    </svg>
+                    <div style="text-align: justify;">
+                        {{ session('successMessage') }}
+                    </div>
+                    <div>
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>
+                </div>
+            @endif
+            @error('successMessage')
+                <div class="alert alert-success alert-dismissible d-flex align-items-center fade show" role="alert">
+                    <svg width="24" height="24" fill="currentColor" class="bi flex-shrink-0 me-2" role="img"
+                        viewBox="0 0 16 16" aria-label="Success:">
+                        <use xlink:href="#check-circle-fill" />
+                    </svg>
+                    <div style="text-align: justify;">
+                        {{ $message }}
+                    </div>
+                    <div>
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>
                 </div>
             @enderror
-        </div>
-        <div class="mb-3">
-            <label for="password" class="form-label text-success">Kata Sandi</label>
-            <input type="password" class="form-control @error('password') is-invalid @enderror" id="password"
-                name="password" placeholder="Masukkan kata sandi anda...">
-            @error('password')
-                <div class="invalid-feedback">
-                    {{ $message }}
+            @if (session()->has('errorMessage'))
+                <div class="alert alert-danger alert-dismissible d-flex align-items-center fade show" role="alert">
+                    <svg width="24" height="24" fill="currentColor" class="bi flex-shrink-0 me-2" role="img"
+                        viewBox="0 0 16 16" aria-label="Success:">
+                        <use xlink:href="#exclamation-triangle-fill" />
+                    </svg>
+                    <div style="text-align: justify;">
+                        {{ session('errorMessage') }}
+                    </div>
+                    <div>
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>
+                </div>
+            @endif
+            @error('errorMessage')
+                <div class="alert alert-danger alert-dismissible d-flex align-items-center fade show" role="alert">
+                    <svg width="24" height="24" fill="currentColor" class="bi flex-shrink-0 me-2" role="img"
+                        viewBox="0 0 16 16" aria-label="Success:">
+                        <use xlink:href="#exclamation-triangle-fill" />
+                    </svg>
+                    <div style="text-align: justify;">
+                        {{ $message }}
+                    </div>
+                    <div>
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>
                 </div>
             @enderror
-        </div>
-        <div class="row">
-            <div class="col-sm-6">
-                <div class="mb-3 form-check">
-                    <input type="checkbox" class="form-check-input" id="remember" name="remember">
-                    <label class="form-check-label text-center text-success" for="exampleCheck1">Ingat saya</label>
+
+            <form action="{{ URL::to('login') }}" method="POST">
+                @csrf
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="form-group">
+                            <label for="email" class="form-label">Email</label>
+                            <input type="email" class="form-control @error('email') is-invalid @enderror" id="email"
+                                name="email" aria-describedby="email" placeholder="Masukkan email anda...">
+                            @error('email')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="col-lg-12">
+                        <div class="form-group">
+                            <label for="password" class="form-label">Kata Sandi</label>
+                            <input type="password" class="form-control @error('password') is-invalid @enderror"
+                                id="password" name="password" aria-describedby="password"
+                                placeholder="Masukkan kata sandi anda...">
+                            @error('password')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="col-lg-12 d-flex justify-content-between">
+                        <div class="form-check mb-3">
+                            <input type="checkbox" class="form-check-input" id="remember" name="remember"
+                                @checked(old('remember')) value="true">
+                            <label class="form-check-label" for="remember">
+                                Ingat Saya
+                            </label>
+                        </div>
+                        <a class="text-success" href="{{ URL::to('forgot-password') }}">Lupa
+                            Password</a>
+                    </div>
                 </div>
-            </div>
-            <div class="col-sm-6 text-end">
-                <a href="{{ URL::to('forgot-password') }}" class="text-success" style="text-decoration: none;">
-                    Lupa Password
-                </a>
-            </div>
+                <div class="d-flex justify-content-center">
+                    <button type="submit" class="btn btn-success">M A S U K</button>
+                </div>
+                <p class="mt-3 text-center">
+                    Belum memiliki akun? <a href="{{ URL::to('register') }}" class="text-underline text-success">Klik
+                        disini untuk daftar.</a>
+                </p>
+            </form>
         </div>
-
-        <div class="d-flex flex-column mb-3">
-            <button type="submit" class="btn btn-success">
-                <i class="fa-solid fa-arrow-right-to-bracket me-1"></i>
-                M A S U K
-            </button>
-        </div>
-
-        <p class="text-center text-success">Tidak memiliki akun? <a href="{{ URL::to('register') }}">Daftar</a></p>
-    </form>
+    </div>
 @endsection
